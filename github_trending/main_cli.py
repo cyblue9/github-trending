b30b43f657627c86bbc465918b97de7ac979b686
Yuya Chiba <cy.blue.9@gmail.com>
2018-08-12 21:58:34 +0900

    Add main entry point in main_cli.py

---
+++ github_trending/main_cli.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright 2018 Yuya Chiba. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+from __future__ import print_function
+
+from .github_trending_cli import GithubTrendingCli
+
+
+def cli():
+    """Creates and calls Git."""
+    github_trending = GithubTrendingCli()
+    github_trending.cli()
+
+
+if __name__ == "__main__":
+    cli()
