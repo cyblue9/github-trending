2e047f6986ac1d78cb770508e67b40ac08794ee6
Yuya Chiba <cy.blue.9@gmail.com>
2018-08-16 23:50:25 +0900

    Add StyleFactory class in style.py

--- github_trending/style.py
+++ github_trending/style.py
@@ -12,3 +12,60 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+
+from pygments.token import Token
+from pygments.util import ClassNotFound
+from prompt_toolkit.styles import default_style_extensions, style_from_dict
+import pygments.styles
+
+
+class StyleFactory(object):
+    """Provide styles for the autocomplete menu and the toolbar.
+
+    :type style: :class:`pygments.style.StyleMeta`
+    :param style: An instance of `pygments.style.StyleMeta`.
+    """
+
+    def __init__(self, name):
+        self.style = self.style_factory(name)
+
+    def style_factory(self, name):
+        """Retrieve the specified pygments style.
+
+        If the specified style is not found, the vim style is returned.
+
+        :type style_name: str
+        :param style_name: The pygments style name.
+
+        :rtype: :class:`pygments.style.StyleMeta`
+        :return: An instance of `pygments.style.StyleMeta`.
+        """
+        try:
+            style = pygments.styles.get_style_by_name(name)
+        except ClassNotFound:
+            style = pygments.styles.get_style_by_name('native')
+
+        # Create styles dictionary.
+        styles = {}
+        styles.update(style.styles)
+        styles.update(default_style_extensions)
+        styles.update({
+            Token.Menu.Completions.Completion.Current: 'bg:#00aaaa #000000',
+            Token.Menu.Completions.Completion: 'bg:#008888 #ffffff',
+            Token.Menu.Completions.Meta.Current: 'bg:#00aaaa #000000',
+            Token.Menu.Completions.Meta: 'bg:#00aaaa #ffffff',
+            Token.Menu.Completions.ProgressButton: 'bg:#003333',
+            Token.Menu.Completions.ProgressBar: 'bg:#00aaaa',
+            Token.Scrollbar: 'bg:#00aaaa',
+            Token.Scrollbar.Button: 'bg:#003333',
+            Token.Toolbar: 'bg:#222222 #cccccc',
+            Token.Toolbar.Off: 'bg:#222222 #696969',
+            Token.Toolbar.On: 'bg:#222222 #ffffff',
+            Token.Toolbar.Search: 'noinherit bold',
+            Token.Toolbar.Search.Text: 'nobold',
+            Token.Toolbar.System: 'noinherit bold',
+            Token.Toolbar.Arg: 'noinherit bold',
+            Token.Toolbar.Arg.Text: 'nobold'
+        })
+
+        return style_from_dict(styles)
