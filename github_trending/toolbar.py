fe21bce8a8d7a39064d691c72314c96bd8bd5d7d
Yuya Chiba <cy.blue.9@gmail.com>
2018-08-16 23:48:21 +0900

    Add Toolbar class in toolbar.py

--- github_trending/toolbar.py
+++ github_trending/toolbar.py
@@ -12,3 +12,39 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+
+from __future__ import print_function
+from pygments.token import Token
+
+
+class Toolbar(object):
+    """Show information about the aws-shell in a tool bar.
+
+    :type handler: callable
+    :param handler: Wraps the callable `get_toolbar_items`.
+    """
+
+    def __init__(self):
+        self.handler = self._create_toolbar_handler()
+
+    def _create_toolbar_handler(self):
+        """Create the toolbar handler.
+
+        :rtype: callable
+        :return: get_toolbar_items.
+        """
+
+        def get_toolbar_items(_):
+            """Return the toolbar items.
+
+            :type _: :class:`prompt_toolkit.Cli`
+            :param _: (Unused)
+
+            :rtype: list
+            :return: A list of (pygments.Token.Toolbar, str).
+            """
+            return [
+                (Token.Toolbar, ' [F10] Exit ')
+            ]
+
+        return get_toolbar_items
